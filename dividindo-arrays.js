ca0fa636f7072974777373e10a65b4168d0edb8f
arturk22 <alencar.artur00@gmail.com>
2022-03-04 10:11:51 -0300

    Usando arrays

---
+++ dividindo-arrays.js
@@ -0,0 +1,8 @@
+const nomes = ['Artur', 'Camila', 'Layla', 'Etinha', 'Laianne']
+
+const sala1 = nomes.slice(0, nomes.length/2)
+const sala2 = nomes.slice(nomes.length/2)
+
+console.log(sala1)
+
+console.log(sala2)
