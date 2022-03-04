627c7f8b4dfb3dcd09eabfe5a16f07c6ee8038fc
arturk22 <alencar.artur00@gmail.com>
2022-03-04 10:37:00 -0300

    Alterando arrays

---
+++ atualizando-lista.js
@@ -0,0 +1,5 @@
+const listaChamada = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Léo']
+
+listaChamada.splice(1, 0, 'Rodrigo')
+
+console.log(`Lista de chamada ${listaChamada}`)
---
+++ lista-de-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana']
+
+const medias = [10, 7, 9, 6]
+
+let listaDeNotasEAlunos = [alunos, medias]
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua media é ${listaDeNotasEAlunos[1][0]}`)
